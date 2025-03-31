14dd54855914ff71834ad3b516bb7b5c3cebf669
DefinitelyUnlikely <Martin@kaarjohansson.se>
2025-03-31 19:41:55 +0200

    Add hamburger css and onClick handler for class change + future opening of sidebar

--- frontend/filesharing/src/components/Header/Hamburger/Hamburger.tsx
+++ frontend/filesharing/src/components/Header/Hamburger/Hamburger.tsx
@@ -1,11 +1,18 @@
 import './Hamburger.css'
+import { useState } from 'react';
 
 export function Hamburger() {
+    const [isActive, SetIsActive] = useState(false);
+
+    const handleOnClick = () => {
+        SetIsActive(!isActive);
+    }
+
     return (
-        <div className="ham-menu">
+        <div className={`ham-menu ${isActive ? "active" : ""}`} onClick={handleOnClick}>
             <span></span>
             <span></span>
             <span></span>
-        </div>
+        </div >
     )
 }
