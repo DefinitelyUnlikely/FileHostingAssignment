eb2762e2cc67dc28f09f6a1b9d319b370f913210
DefinitelyUnlikely <Martin@kaarjohansson.se>
2025-03-31 19:24:49 +0200

    Init commit for ASP.NET backend and react+vite based frontend

---
+++ frontend/filesharing/src/components/Header/Hamburger/Hamburger.tsx
@@ -0,0 +1,11 @@
+import './Hamburger.css'
+
+export function Hamburger() {
+    return (
+        <div className="ham-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
