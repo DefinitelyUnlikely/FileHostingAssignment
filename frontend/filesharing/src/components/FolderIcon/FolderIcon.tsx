3af95b08fe1c816d7ee78c4d78862485cffc5b3a
DefinitelyUnlikely <Martin@kaarjohansson.se>
2025-04-02 10:53:45 +0200

    Add paragraph and some css to folder icon

--- frontend/filesharing/src/components/FolderIcon/FolderIcon.tsx
+++ frontend/filesharing/src/components/FolderIcon/FolderIcon.tsx
@@ -1,10 +1,16 @@
+import './FolderIcon.css'
+
+
 export function FolderIcon() {
+
+
     return (
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-            <g id="SVGRepo_iconCarrier">
-                <path d="
+        <div className="folder-icon">
+            <svg viewBox="0 0 24 24" height="5rem" width="5rem" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                    <path d="
             M3 8.2C3 7.07989 3 6.51984 3.21799 6.09202C3.40973 5.71569 3.71569
             5.40973 4.09202 5.21799C4.51984 5 5.0799 5 6.2 5H9.67452C10.1637 5
             10.4083 5 10.6385 5.05526C10.8425 5.10425 11.0376 5.18506 11.2166 
@@ -17,10 +23,12 @@ export function FolderIcon() {
             18.782C19.4802 19 18.9201 19 17.8 19H6.2C5.07989 19 4.51984 
             19 4.09202 18.782C3.71569 18.5903 3.40973 18.2843 3.21799 17.908C3 
             17.4802 3 16.9201 3 15.8V8.2Z"
-                    stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                </path>
-            </g>
-        </svg>
+                        stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+                    </path>
+                </g>
+            </svg>
+            <p className="folder-text">Hello</p>
+        </div>
     )
 }
 
