6fbe092c3371dba3a186c2db3566c840451c3e06
DefinitelyUnlikely <Martin@kaarjohansson.se>
2025-04-03 13:33:28 +0200

    Added more folder icons to test grid css

--- frontend/filesharing/src/pages/HomePage.tsx
+++ frontend/filesharing/src/pages/HomePage.tsx
@@ -2,9 +2,20 @@ import './HomePage.css'
 import { FolderIcon } from '../components/FolderIcon/FolderIcon'
 
 export function Homepage() {
+
+
+
     return (
         <main className="home-page">
-            <FolderIcon />
+            <div className="folder-grid">
+                <FolderIcon folderName='Folder 1' />
+                <FolderIcon folderName='Folder 2' />
+                <FolderIcon folderName='Folder 3' />
+                <FolderIcon folderName='Folder 4' />
+                <FolderIcon folderName='Folder 5' />
+                <FolderIcon folderName='Folder 6' />
+            </div>
+
         </main>
     )
 }
